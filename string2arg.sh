38158d49d8ea6632e8903889040aab64ebc32052
Magnushhoie <39849954+Magnushhoie@users.noreply.github.com>
2023-10-07 23:54:24 +0200

    More centered preview window line

--- string2arg.sh
+++ string2arg.sh
@@ -5,12 +5,14 @@ string2arg() {
     export arg_filename=$(cut -d":" -f1 <<< "$*");
     export arg_linenum=$(cut -d":" -f2 <<< "$*");
 
-    min_offset=25
+    min_offset=12
     let max_offset="min_offset*3"
     min=0
     if (($min_offset < $arg_linenum)); then
         let min="arg_linenum-$min_offset"
     fi
     let max="arg_linenum+$max_offset"
-    bat --color=always --highlight-line "$arg_linenum" --style=header,grid,numbers --line-range "$min":"$max" "$arg_filename";
+    bat --color=always --highlight-line "$arg_linenum" \
+    --style=header,grid,numbers \
+    --line-range "$min":"$max" "$arg_filename";
 }
